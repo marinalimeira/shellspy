67b1b1d31d3635007a1169edbde30f990a717b75
Marina Limeira <marina@gruntwork.io>
2023-01-13 16:09:52 +0100

    pairing 13. january

--- shellspy.go
+++ shellspy.go
@@ -4,9 +4,21 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"os/exec"
+	"strings"
 )
 
-// Your implementation goes here!
+// Object in Go means the struct.
+
+func ExecCommand(command *exec.Cmd) string {
+	return ""
+}
+
+// CommandFromString returns a CommandObject from input line.
+func CommandFromString(command string) exec.Cmd {
+	args := strings.Split(command, " ")
+	return exec.Cmd{Args: args}
+}
 
 func Reads(in io.Reader, out io.Writer) {
 	input := bufio.NewScanner(in) // we know its a scanner, but it reads an input
@@ -14,5 +26,6 @@ func Reads(in io.Reader, out io.Writer) {
 	// 1. execute the command and output it.
 	for input.Scan() {
 		fmt.Fprint(out, input.Text())
+
 	}
 }
--- shellspy_test.go
+++ shellspy_test.go
@@ -2,12 +2,14 @@ package shellspy_test
 
 import (
 	"bytes"
+	"github.com/google/go-cmp/cmp"
 	"github.com/marinalimeira/shellspy"
+	"os/exec"
 	"strings"
 	"testing"
 )
 
-func TestReads(t *testing.T) {
+func TestReads_ExecutesTheInputsAsCommands(t *testing.T) {
 	reader := strings.NewReader("echo hi!")
 
 	out := bytes.Buffer{}
@@ -17,6 +19,75 @@ func TestReads(t *testing.T) {
 	want := "hi!"
 
 	if got != want {
-		t.Fatalf("%s and %s are not equal", got, want)
+		// With %q it shows the string with quotes
+		t.Fatalf("Want: %q\nGot: %q", want, got)
+	}
+}
+
+func TestReads_IgnoresWhenItsEmpty(t *testing.T) {
+	reader := strings.NewReader("")
+
+	out := bytes.Buffer{}
+	shellspy.Reads(reader, &out)
+
+	got := out.String()
+	want := ""
+
+	if got != want {
+		// With %q it shows the string with quotes
+		t.Fatalf("Want: %q\nGot: %q", want, got)
+	}
+}
+
+func TestCommandFromString_ReturnsCommandObjectFromInputLine(t *testing.T) {
+	// Given a string,
+	// It will return the command to be executed, and it's args.
+
+	testCase := []struct {
+		input string
+		want  []string
+	}{
+		{
+			input: "echo hi!",
+			want:  []string{"echo", "hi!"},
+		},
+		{
+			input: "echo",
+			want:  []string{"echo"},
+		},
+		{
+			input: "abc",
+			want:  []string{"abc"},
+		},
+	}
+
+	for _, test := range testCase {
+		got := shellspy.CommandFromString(test.input)
+
+		// In this case, no need to compare the whole struct, only the attrs that are changing
+
+		if !cmp.Equal(test.want, got.Args) {
+			t.Errorf("Input: %q\nDiff: %s", test.input, cmp.Diff(test.want, got.Args))
+		}
+	}
+}
+
+// 1. Write phrase with what will happen
+func TestExecCommand_RunsAndReturnsTheOutputGivenOfCommand(t *testing.T) {
+	// 2. Write test that does whatever "phrase" above says
+
+	// it returns a pointer, something changes.
+	// Entity.
+	// Read-only.
+	//
+	command := exec.Command("echo", "hi!")
+
+	// 3. Start from middle
+	got := shellspy.ExecCommand(command)
+
+	want := "hi!"
+
+	if got != want {
+		t.Errorf("Input: \"echo hi!\"\n Diff: %s", cmp.Diff(want, got))
 	}
 }
